cf86de067703eb428457b952602f8bbdc81b0ff0
SamirShef <ssserin10@gmail.com>
2026-02-23 00:06:37 +0300

    Added minimal skeleton

---
+++ build/CMakeFiles/tvm.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tvm.dir/link.d"
+  "CMakeFiles/tvm.dir/src/main.c.o"
+  "CMakeFiles/tvm.dir/src/main.c.o.d"
+  "tvm"
+  "tvm.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/tvm.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int
+main(int argc, char **argv) {
+    puts("Entry point of Talc Programming Language (TVM)");
+    return 0;
+}
